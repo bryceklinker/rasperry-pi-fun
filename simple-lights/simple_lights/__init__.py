6a8097668c6bec36cc9dfe85a81c7b793601c1c9
Bryce Klinker <bryce.klinker@gmail.com>
2019-12-24 12:06:23 -0600

    initial version of lighting up a single light for one second

---
+++ simple-lights/simple_lights/__init__.py
@@ -0,0 +1,15 @@
+import time
+import RPi.GPIO as GPIO
+
+
+def start_lighting(time=time, gpio=GPIO):
+    gpio.setmode(GPIO.BCM)
+    gpio.setwarnings(False)
+    gpio.setup(18, GPIO.OUT)
+    print("LED on")
+
+    gpio.output(18, GPIO.HIGH)
+    time.sleep(1)
+
+    print("LED off")
+    gpio.output(18, GPIO.LOW)
---
+++ simple-lights/tests/test_simple_lights.py
@@ -0,0 +1,26 @@
+
+def test_when_ran_then_pin_18_is_lit_up():
+    time = FakeTime()
+    gpio = FakeGPIO()
+
+    start_lighting(time, gpio)
+
+    
+
+class FakeTime:
+    def sleep(time):
+        pass
+
+class FakeGPIO:
+    def setmode(mode):
+        pass
+
+    def setwarnings(show_warnings):
+        pass
+
+    def setup(pin, in_or_out):
+        pass
+
+    def output(pin, level):
+        pass
+    
