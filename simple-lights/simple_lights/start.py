e8b6d0ce393a1e6a5c5a24c463cd0b1c647ba9df
Bryce Klinker <bryce.klinker@gmail.com>
2019-12-25 14:49:53 -0600

    Adding second set of lights

--- simple-lights/simple_lights/start.py
+++ simple-lights/simple_lights/start.py
@@ -2,9 +2,12 @@ from gpiozero import LED
 from time import sleep
 
 pin_18_led = LED(18)
+pin_17_led = LED(17)
 
 while True:
+    pin_17_led.off()
     pin_18_led.on()
     sleep(1)
+    pin_17_led.on()
     pin_18_led.off()
     sleep(1)
